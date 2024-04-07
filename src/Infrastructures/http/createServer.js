a08665d05882173bf846a0f547c8ee6af505b8f6
savareyhano <savareyhano99@gmail.com>
2024-04-07 16:24:15 +0700

    trigger ci fail

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -59,10 +59,6 @@ const createServer = async (container) => {
       plugin: replies,
       options: { container },
     },
-    {
-      plugin: likes,
-      options: { container },
-    },
   ]);
 
   server.ext('onPreResponse', (request, h) => {
